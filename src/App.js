cf563236e829442f93dfe6fd50092473fdce12c5
tarrinros <tarindis@gmail.com>
2019-10-25 16:03:41 +0300

    Add Quiz container with styles module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
+import Quiz from './containers/Quiz/Quiz';
 
 function App() {
   return (
     <Layout>
-      <div style={{width: '400px', border: '1px solid black', textAlign: 'center'}}>
-        <h1>Layout works</h1>
-      </div>
+      <Quiz/>
     </Layout>
   );
 }
