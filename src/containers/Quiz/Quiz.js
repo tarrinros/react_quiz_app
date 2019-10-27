f92a17c9f921b649c433f576047ef15cce360686
tarrinros <tarindis@gmail.com>
2019-10-27 19:02:36 +0200

    Add setState isFinished when quiz in finished

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,6 +6,7 @@ class Quiz extends Component {
   state = {
     answerState: null,
     activeQuestion: 0,
+    isFinished: false,
     quiz: [
       {
         question: 'Какого цвета небо?',
@@ -63,7 +64,9 @@ class Quiz extends Component {
       // Returns the message if answer is correct and clears timeout to avoid memory leak
       const timeout = window.setTimeout(() => {
         if (this.isQuizFinished()) {
-          console.log('Finished')
+          this.setState({
+            isFinished: true
+          })
         } else {
           // Switches question to the next one
           this.setState({
@@ -90,15 +93,18 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Ответьте на все вопросы</h1>
-
-          <ActiveQuiz
-            question={this.state.quiz[this.state.activeQuestion].question}
-            answers={this.state.quiz[this.state.activeQuestion].answers}
-            onAnswerClick={this.onAnswerClickHandler}
-            quizLength={this.state.quiz.length}
-            answerNumber={this.state.activeQuestion + 1}
-            answerState={this.state.answerState}
-          />
+          {
+            this.state.isFinished
+              ? <h1>Finished</h1>
+              : <ActiveQuiz
+                question={this.state.quiz[this.state.activeQuestion].question}
+                answers={this.state.quiz[this.state.activeQuestion].answers}
+                onAnswerClick={this.onAnswerClickHandler}
+                quizLength={this.state.quiz.length}
+                answerNumber={this.state.activeQuestion + 1}
+                answerState={this.state.answerState}
+              />
+          }
         </div>
       </div>
     );
