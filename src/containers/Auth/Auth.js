23406004c7bf5b141d34d977c493bef2790c2746
tarrinros <tarindis@gmail.com>
2019-10-29 17:16:36 +0200

    Add functionality to Auth container

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import classes from './Auth.module.css'
+import Button from "../../components/UI/Button/Button";
 
 export default class Auth extends Component {
+  loginHandler = () => {
+
+  }
+
+  signUpHandler = () => {
+
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Autherization</h1>
+          <form onSubmit={this.submitHandler}>
+            <input type="text"/>
+            <input type="text"/>
+
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >Login</Button>
+            <Button
+              type="primary"
+              onClick={this.signUpHandler}
+            >Sign up</Button>
+          </form>
+        </div>
       </div>
     )
   }
