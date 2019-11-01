3cfce32965b0c930162689dafe1cd9317b99d960
tarrinros <tarindis@gmail.com>
2019-11-01 10:35:16 +0200

    Add initial functional for CreateQuiz component

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import classes from './QuizCreator.module.css'
+import Button from "../../components/UI/Button/Button";
 
 export default class QuizCreator extends Component {
+  submitHandler = (event) => {
+    event.preventDefault()
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  createQuizHandler = () => {
+    
+  }
+
   render() {
     return (
-      <div>
-        <h1>QuizCreator</h1>
+      <div className={classes.QuizCreator}>
+        <h1>Create the quiz</h1>
+
+        <form onSubmit={this.submitHandler}></form>
+        <select></select>
+
+        <Button
+          type="primary"
+          onClick={this.addQuestionHandler}
+        >
+          Add question
+        </Button>
+
+        <Button
+          type="success"
+          onClick={this.createQuizHandler}
+        >
+          Create quiz
+        </Button>
       </div>
     )
   }
