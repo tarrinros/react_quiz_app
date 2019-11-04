a768475ff63fdf8c7ce41def0240f7b783ee786a
tarrinros <tarindis@gmail.com>
2019-11-04 23:21:06 +0200

    Add api key to the local env variable

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
 import classes from './Auth.module.css'
-import Button from "../../components/UI/Button/Button";
+import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input"
 import is from 'is_js'
+import axios from 'axios'
 
-// use 'is_js' module instead this function
+// use 'is_js' module for email validation instead this function
 // function validateEmail(email) {
 //   const re = /^[^@]+@[^@]+$/i;
 //   return re.test(String(email).toLowerCase());
 // }
+const USERS_SECRET = process.env.REACT_APP_AUTH_KEY
 
 export default class Auth extends Component {
   state = {
@@ -39,26 +41,48 @@ export default class Auth extends Component {
         }
       }
     }
-  }
+  };
 
-  loginHandler = () => {
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
+    try {
+      const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${USERS_SECRET}`, authData)
+      console.log(USERS_SECRET)
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  };
 
-  }
-
-  signUpHandler = () => {
-
-  }
+  signUpHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
+    try {
+      const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${USERS_SECRET}`, authData);
+      console.log(response.data)
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  };
 
   submitHandler = event => {
     event.preventDefault()
-  }
+  };
 
-  valdateControl(value, validation) {
+  validateControl(value, validation) {
     if (!validation) {
       return true
     }
 
-    let isValid = true
+    let isValid = true;
 
     if (validation.required) {
       isValid = value.trim() !== '' && isValid
@@ -77,27 +101,27 @@ export default class Auth extends Component {
   }
 
   onChangeHandler = (event, controlName) => {
-    const formControls = {...this.state.formControls}
-    const control = {...formControls[controlName]}
+    const formControls = {...this.state.formControls};
+    const control = {...formControls[controlName]};
 
-    control.value = event.target.value
-    control.touched = true
-    control.valid = this.valdateControl(control.value, control.validation)
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
 
-    formControls[controlName] = control
+    formControls[controlName] = control;
 
-    let isFormValid = true
+    let isFormValid = true;
     Object.keys(formControls).forEach(name => {
       isFormValid = formControls[name].valid && isFormValid
-    })
+    });
     this.setState({
       formControls, isFormValid
     })
-  }
+  };
 
   renderInputs() {
     return Object.keys(this.state.formControls).map((controlName, index) => {
-      const control = this.state.formControls[controlName]
+      const control = this.state.formControls[controlName];
 
       return (
         <Input
