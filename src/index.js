55068d954876cd3de3f3e77e26c7306855d17fe4
tarrinros <tarindis@gmail.com>
2019-11-05 17:39:02 +0200

    Add middelware support to the index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import {Provider} from 'react-redux'
 import rootReducer from "./store/reducers/rootReducer";
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer)
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
 const app = (
   <Provider store={store}>
     <BrowserRouter>
@@ -16,5 +30,6 @@ const app = (
     </BrowserRouter>
   </Provider>
 );
+
 ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
