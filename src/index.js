ae953112a281e41a41b40b79c94fc8bd9e400a88
tarrinros <tarindis@gmail.com>
2019-11-05 16:51:10 +0200

    Create and import rootReducer with combineReducers from redux

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
 import {createStore} from "redux";
 import {Provider} from 'react-redux'
+import rootReducer from "./store/reducers/rootReducer";
 
-const store = createStore()
+const store = createStore(rootReducer)
 const app = (
   <Provider store={store}>
     <BrowserRouter>
