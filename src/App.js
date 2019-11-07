77ab9324df3491a6e25cf58cb63efdd75ce42dfd
tarrinros <tarindis@gmail.com>
2019-11-08 00:12:51 +0200

    Change backdrop panel links depens on authorization status

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Quiz from './containers/Quiz/Quiz';
 import QuizList from './containers/QuizList/QuizList';
 import QuizCreator from './containers/QuizCreator/QuizCreator';
 import Auth from './containers/Auth/Auth';
+import {connect} from "react-redux";
+import Logout from "./components/Logout/Logout";
 
 function App() {
-  return (
-    <Layout>
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth}/>
+      <Route path="/quiz-creator" component={QuizCreator}/>
+      <Route path="/quiz/:id" component={Quiz}/>
+      <Route path="/" component={QuizList}/>
+      <Redirect to='/'/>
+    </Switch>
+  )
+
+  if (this.props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path="/auth" component={Auth}/>
         <Route path="/quiz-creator" component={QuizCreator}/>
         <Route path="/quiz/:id" component={Quiz}/>
         <Route path="/" component={QuizList}/>
+        <Route path="/logout" component={Logout}/>
+        <Redirect to='/'/>
       </Switch>
+    )
+  }
+
+  return (
+    <Layout>
+      {routes}
     </Layout>
   );
+
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+export default connect(mapStateToProps)(App);
